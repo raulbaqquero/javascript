fe18417349007b81f4091c7b8098b12cf8aac80b
Raúl Baquero <93211537+raulbaqquero@users.noreply.github.com>
2021-12-10 13:51:27 +0100

    Add files via upload

---
+++ ejercicio3_mates/escripito.js
@@ -0,0 +1,33 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+var celta = 1.1234 ;
+    var raizCelta = Math.sqrt(celta);
+    alert(raizCelta);
+
+var madrid = 5.5678 ;
+    var raizMadrid = Math.sqrt(madrid);
+    alert(raizMadrid);
+
+var sevilla = 9.9123 ;
+    var raizSevilla = Math.sqrt(sevilla);
+    alert(raizSevilla);
+
+var potencia = 23 ;
+    var cosita = Math.pow(potencia,2);
+    alert(cosita);
+
+var hola = Math.PI ;
+    var pi = Math.trunc(Math.PI);
+    alert(pi);
+
+var minimo = Math.min ;
+    var esloquehay = Math.min(celta, madrid, sevilla);
+    alert(esloquehay);
+
+var numero = Math.random ;
+    var aleatorio = Math.random(0,3);
+    alert(aleatorio);
